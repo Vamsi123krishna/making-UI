370687bf9615d18b14926a5d837abf42e4ad89ce
Vamsi123krishna <96591539+Vamsi123krishna@users.noreply.github.com>
2024-02-25 17:54:29 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Signuppage from "./components/Signuppage";
+import Homepage from "./components/Homepage";
+import Whiteplainpage from "./components/whiteplainpage";
+import "./styles.css";
+
+const App = () => (
+  <div className="App">
+    <Signuppage />
+    <Homepage />
+    <Whiteplainpage />
+  </div>
+);
+export default App;
