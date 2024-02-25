05e83107915b6d73ea44c85bdd5dea80375d2cfd
Vamsi123krishna <96591539+Vamsi123krishna@users.noreply.github.com>
2024-02-25 18:02:23 +0530

    final commit

---
+++ src/components/whiteplainpage/index.jsx
@@ -0,0 +1,9 @@
+const Whiteplainpage = () => {
+  return (
+    <div className="whiteplain-container">
+      <h1 className="heading">Tushar</h1>
+    </div>
+  );
+};
+
+export default Whiteplainpage;
